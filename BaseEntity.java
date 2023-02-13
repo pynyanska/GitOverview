ad405c19cc2f9453691d1c130e142cae39a4f2e7
Nina Pynyanska <nina.pynyanska@avast.com>
2023-02-13 15:19:00 +0200

    Adding new files

---
+++ BaseEntity.java
@@ -0,0 +1,21 @@
+public class BaseEntity {
+    private long id;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "BaseEntity{" +
+                "id=" + id +
+                '}';
+    }
+    public boolean isNew(){
+        return  (this.id == null);
+    }
+}
